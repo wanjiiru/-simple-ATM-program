22f0716c0e780e59eacc3e0021c8cab216ae0db7
Wanjiru Nganga <wnganga@farmdrive.co.ke>
2019-06-13 09:37:37 +0300

    add deposit option

--- atm.py
+++ atm.py
@@ -1,4 +1,4 @@
-balance = 50.00
+balance = 100000
 print("    ATM    ")
 
 
@@ -11,6 +11,17 @@ print("""
 
 """)
 
+
+def withdraw(balance,amount,max_per_transaction=20000):
+    if balance<0:
+        balance=balance-10
+    else:
+        balance=balance-amount
+    return balance
+
+
+
+
 Choice =int(input("Enter Option: "))
 
 if Choice==1:
@@ -19,22 +30,25 @@ if Choice==1:
 if Choice==2:
     print(" Your Balance is  KES ",balance)
     withdraw_amount=float(input("Please enter amount to withdraw "))
-    if withdraw_amount>0 and withdraw_amount<balance:
-        amount=(balance-withdraw_amount)
-        print("Your balance is   kes ",amount)
+
+    if withdraw_amount>20000:
+        print('Maximum amount per transaction is 20000')
+
+    elif withdraw_amount>1 and withdraw_amount<balance:
+        amount_remaining=withdraw(balance,amount=withdraw_amount)
+        print("Your balance is   kes ",amount_remaining)
+
     elif withdraw_amount>balance:
         print("Not enough funds in your account")
 
 elif Choice==3:
     print(" Your Balance is  KES ", balance)
     deposit_amount = float(input("Enter amount to deposit "))
-        if Deposit > 0:
-            forewardbalance = (balance + Deposit)
-            print("Forewardbalance  £ ", forewardbalance)
-        else:
-            print("None deposit made")
+    if deposit_amount>1 and deposit_amount<50000:
+        new_balance=(balance+deposit_amount)
+        print("Your balance is   kes ",new_balance)
+    else:
+        print('Please enter an amount between 1 and 50000')
 
-    if Option == 4:
-        exit()
 
 
