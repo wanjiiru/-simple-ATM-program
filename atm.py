509fa974785b7a0d938ebfdda15c31aa3c559573
Wanjiru Nganga <wnganga@farmdrive.co.ke>
2019-06-13 09:54:12 +0300

    add exit prompt

--- atm.py
+++ atm.py
@@ -19,6 +19,11 @@ def withdraw(balance,amount,max_per_transaction=20000):
         balance=balance-amount
     return balance
 
+def deposit(balance, amount):
+    if amount>40000:
+        print('enter right amount')
+    else:
+        balance=+amount
 
 
 
@@ -37,18 +42,28 @@ if Choice==2:
     elif withdraw_amount>1 and withdraw_amount<balance:
         amount_remaining=withdraw(balance,amount=withdraw_amount)
         print("Your balance is   kes ",amount_remaining)
+        exit()
 
     elif withdraw_amount>balance:
         print("Not enough funds in your account")
+        exit()
 
 elif Choice==3:
     print(" Your Balance is  KES ", balance)
     deposit_amount = float(input("Enter amount to deposit "))
-    if deposit_amount>1 and deposit_amount<50000:
+    if deposit_amount>1 and deposit_amount<40000:
         new_balance=(balance+deposit_amount)
         print("Your balance is   kes ",new_balance)
     else:
         print('Please enter an amount between 1 and 50000')
 
 
+
+elif Choice==4:
+    options=[1,2]
+    ask=input("""
+    are you sure you want to exit
+    """)
+    if ask ==1 or 'yes':
+        exit()
 
